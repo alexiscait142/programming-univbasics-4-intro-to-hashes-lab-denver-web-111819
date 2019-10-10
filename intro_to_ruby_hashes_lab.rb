57bf53f2f9e39ac6d000c93d088d11fbc2a68450
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-10 04:31:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,12 +23,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  update_hash = {}
-  
   if hash[key]
     hash[key] += 1
   else 
     hash[key] = 1
   end
-  update_hash(hash[key])
 end
